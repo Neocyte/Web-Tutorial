0ea03c2a7cb23312592033b8f348cc5b3f208315
Mason <masonwu8462@yahoo.com>
2020-08-31 00:48:31 -0400

    Removed unnecessary console logs

--- page.js
+++ page.js
@@ -5,10 +5,7 @@ window.onscroll = () => {
     Array.from(cards).slice(2, 4).forEach((card) => {
         let bottom_of_object = card.offsetTop;
         let bottom_of_window = window.scrollY + window.innerHeight;
-        console.log(bottom_of_window + " vs " + bottom_of_object);
-
         if( bottom_of_window > bottom_of_object ){
-            console.log("hello");
             card.style.opacity = "1";
         }
     });
